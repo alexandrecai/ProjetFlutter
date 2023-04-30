5dc6570baf5c60f16b398205911179469f2649bc
Barry Fatoumata <fatoumata.barry1@etu.univ-orleans.fr>
2023-04-30 20:31:52 +0200

    Liaison  BDD et BIBLIOTHEQUE

--- lib/Pages/BookListPage.dart
+++ lib/Pages/BookListPage.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
 import 'package:projetflutter/Pages/BookDetailsPage.dart';
 import 'package:projetflutter/Pages/SearchBarDelegate.dart';
+import 'package:sqflite/sqflite.dart';
 
+import '../Controllers/HttpServiceBook.dart';
 import '../Objects/Author.dart';
 import '../Objects/Book.dart';
 import '../Objects/Categorie.dart';
@@ -16,8 +19,14 @@ class BookListPage extends StatefulWidget {
 
 class _BookListPageState extends State<BookListPage> {
 
+  HttpServiceBook liste = HttpServiceBook();
+  List<Book> books=[];
+  /*Future<void> fetchBooks() async {
+    books = await liste.getAllBook();
 
-  List<Book> books = [
+  }*/
+
+  /*List<Book> books = [
     Book(1,     "The Great Gatsby", "The story of the mysteriously wealthy Jay Gatsby and his love for the beautiful Daisy Buchanan, of lavish parties on Long Island at a time when The New York Times noted 'gin was the national drink and sex the national obsession,' it is an exquisitely crafted tale of America in the 1920s.",
         Categorie(1,"Classic Fiction"), MaisonEdition(1,"Scribner",[],[]), Author(1, "Scott", "Scott Fitzgerald"),
         "lee", "978-0-06-112008-4", true, 1925, []),
@@ -25,7 +34,7 @@ class _BookListPageState extends State<BookListPage> {
       MaisonEdition(2,"HarperCollins",[],[]), Author(2,"Harper" ,"Lee"), "Lee", "978-0-06-112008-4", true, 1960, [],),
     Book(3, "The Hitchhiker's Guide to the Galaxy", "Seconds before the Earth is demolished to make way for a galactic freeway, Arthur Dent is plucked off the planet by his friend Ford Prefect, a researcher for the revised edition of The Hitchhiker's Guide to the Galaxy who, for the last fifteen years, has been posing as an out-of-work actor. Together this dynamic pair begin a journey through space aided by quotes from The Hitchhiker's Guide ('A towel is about the most massively useful thing an interstellar hitchhiker can have') and a galaxy-full of fellow travelers: Zaphod Beeblebrox—the two-headed, three-armed ex-hippie and totally out-to-lunch president of the galaxy; Trillian, Zaphod's girlfriend (formally Tricia McMillan), whom Arthur tried to pick up at a cocktail party once upon a time zone; Marvin, a paranoid, brilliant, and chronically depressed robot; Veet Voojagig, a former graduate student who is obsessed with the disappearance of all the ballpoint pens he bought over the years.", Categorie(3,"Science Fiction"), MaisonEdition(3,"Del Rey Books",[],[]),
       Author(3,"Douglas", "Adams"), "Adams", "978-0-345-39181-9", true, 1979, [],
-    )  ];
+    )  ];*/
 
 
   @override
@@ -46,8 +55,50 @@ class _BookListPageState extends State<BookListPage> {
       },
     ),
 
+    body: FutureBuilder<List<Book>>(
+      future: liste.getAllBook(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          List<Book> books = snapshot.data!;
+          return ListView.builder(
+            itemCount: books.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: const Icon(Icons.book),
+                title: Text(books[index].name),
+                subtitle: Text(books[index].description),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => BookDetailsPage(
+                        title: books[index].name,
+                        author: books[index].author.nom,
+                        description: books[index].description,
+                        year: books[index].releaseYear.toString(),
+                        isbn: books[index].ISBN,
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+          );
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Erreur lors du chargement des données'));
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      },
 
-    body: ListView.builder(
+    ),
+  );
+
+
+
+
+
+    /*body: ListView.builder(
       itemCount: books.length,
       itemBuilder: (context, i) => GestureDetector(
         onTap: () {
@@ -72,7 +123,9 @@ class _BookListPageState extends State<BookListPage> {
 
       ),
     ),
+*/
 
-  );
+
+
 
 }
